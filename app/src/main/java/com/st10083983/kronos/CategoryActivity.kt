6f152fc27c05bd24d2e21c04be442068a482b8a8
Thakshin N <thakshinnaidoo@gmail.com>
2023-06-06 23:58:35 +0200

    Back Buttons

--- app/src/main/java/com/st10083983/kronos/CategoryActivity.kt
+++ app/src/main/java/com/st10083983/kronos/CategoryActivity.kt
@@ -1,11 +1,23 @@
 package com.st10083983.kronos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class CategoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category)
+
+        // Back Button
+        val navBackHome = findViewById<Button>(R.id.category_back_button)
+
+        navBackHome.setOnClickListener()
+        {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
--- app/src/main/java/com/st10083983/kronos/TimesheetActivity.kt
+++ app/src/main/java/com/st10083983/kronos/TimesheetActivity.kt
@@ -1,11 +1,23 @@
 package com.st10083983.kronos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class TimesheetActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_timesheet)
+
+        // Back Button
+        val navBackHome = findViewById<Button>(R.id.timesheet_back_button)
+
+        navBackHome.setOnClickListener()
+        {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
