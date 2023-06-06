6a3858aee88157fd4d82a5f5e5fd8f10e5e8b313
RivelleG <ST10083472@vcconnect.edu.za>
2023-06-07 00:04:05 +0200

    Added comment

--- app/src/main/java/com/st10083983/kronos/TimesheetActivity.kt
+++ app/src/main/java/com/st10083983/kronos/TimesheetActivity.kt
@@ -18,6 +18,7 @@ class TimesheetActivity : AppCompatActivity() {
             val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
             finish()
+            //Testing
         }
     }
 }
