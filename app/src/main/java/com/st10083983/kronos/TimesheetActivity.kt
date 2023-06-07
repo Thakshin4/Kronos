6dec9890a879bb61ca85c2b5079bc1cf92ac8414
RivelleG <ST10083472@vcconnect.edu.za>
2023-06-07 12:38:14 +0200

    Added Datepicker For TimesheetActivity

--- app/src/main/java/com/st10083983/kronos/TimesheetActivity.kt
+++ app/src/main/java/com/st10083983/kronos/TimesheetActivity.kt
@@ -1,5 +1,6 @@
 package com.st10083983.kronos
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,17 +8,57 @@ import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Spinner
+import android.widget.TextView
 import android.widget.Toast
 import java.util.Date
 import java.text.SimpleDateFormat
+import java.util.Calendar
 
 // ArrayList of KTimesheet Objects
 var arrEntries = arrayListOf<KTimesheet>()
 
 class TimesheetActivity : AppCompatActivity() {
+    lateinit var pickDateBtn: Button
+    lateinit var selectedDateTV: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_timesheet)
+        pickDateBtn = findViewById(R.id.idBtnPickDate)
+        selectedDateTV = findViewById(R.id.idTVSelectedDate)
+        // on below line we are adding
+        // click listener for our button
+        pickDateBtn.setOnClickListener {
+            // on below line we are getting
+            // the instance of our calendar.
+            val c = Calendar.getInstance()
+
+            // on below line we are getting
+            // our day, month and year.
+            val year = c.get(Calendar.YEAR)
+            val month = c.get(Calendar.MONTH)
+            val day = c.get(Calendar.DAY_OF_MONTH)
+
+            // on below line we are creating a
+            // variable for date picker dialog.
+            val datePickerDialog = DatePickerDialog(
+                // on below line we are passing context.
+                this,
+                { view, year, monthOfYear, dayOfMonth ->
+                    // on below line we are setting
+                    // date to our text view.
+                    selectedDateTV.text =
+                        (dayOfMonth.toString() + "-" + (monthOfYear + 1) + "-" + year)
+                },
+                // on below line we are passing year, month
+                // and day for the selected date in our date picker.
+                year,
+                month,
+                day
+            )
+            // at last we are calling show
+            // to display our date picker dialog.
+            datePickerDialog.show()
+        }
 
         // Spinner
         val arrCategoryNames = arrCategories.map { it.categoryName }
