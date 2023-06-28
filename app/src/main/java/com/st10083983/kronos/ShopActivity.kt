733371262ea86df33905327fceb129bdc5e0db5e
Thakshin N <thakshinnaidoo@gmail.com>
2023-06-28 18:38:04 +0200

    Created Timer and Shop Activity Updated Home Activity

---
+++ app/src/main/java/com/st10083983/kronos/ShopActivity.kt
@@ -0,0 +1,11 @@
+package com.st10083983.kronos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShopActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop)
+    }
+}
